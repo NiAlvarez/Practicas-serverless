4aa7bfeda433714180e00ec53ea7ed2e760a220c
Alan <aeberdinelli@gmail.com>
2021-03-11 11:05:10 -0300

    Refactored handler

--- handler.js
+++ handler.js
@@ -1,56 +1,56 @@
-let codeResponse = require("./utils/statusCode");
+const codeResponse = require("./utils/statusCode");
 
-module.exports.main = (event, context, callback) => {
-    let { transactions } = JSON.parse(event.body);
-    let statusCode = 200;
-    let dolar = 90.58;
+async function handler(event, context) {
+    const { transactions } = JSON.parse(event.body);
+    const dolar = 90.58 + Math.random();
     let totalEnPesos = 0;
 
     //si no existe el parametro
 
-    if (transactions === undefined) {
-        response = codeResponse(
-            400,
-            "Bad request due to missing a required parameter"
-        );
-    } else {
-        //bucle de transacciones
-
-        for (let i = 0; i < transactions.lenght; i++) {
-            //verifica que las transacciones sean USD u ARS y no otra
-            if (
-                transactions[i].amount < 0 ||
-                transactions[i].amount === "" ||
-                (transactions[i].amount !== "USD" &&
-                    transactions[i].amount !== "ARS")
-            ) {
-                statusCode = 500;
-            } else {
-                //hace el balance general
-                transactions[i].currency === "ARS" ?
-                    (totalEnPesos += parseFloat(transactions[i].amount)) :
-                    (totalEnPesos += parseFloat(transactions[i].amount) * dolar);
-            }
+    if (!transactions) {
+        return {
+            statusCode: 400,
+            body: 'Bad request due to missing a required parameter'
         }
+    }
+     
+    //bucle de transacciones
 
-        //Si el status es 500
-
-        if (statusCode === 500) {
-            response = codeResponse(500, "Internal Server Error");
+    for (let i = 0; i < transactions.lenght; i++) {
+        //verifica que las transacciones sean USD u ARS y no otra
+        if (
+            transactions[i].amount < 0 ||
+            transactions[i].amount === "" ||
+            (transactions[i].amount !== "USD" &&
+                transactions[i].amount !== "ARS")
+        ) {
+            statusCode = 500;
         } else {
-            //si la operacion fue exitosa
-            response = {
-                body: JSON.stringify({
-                    status: 200,
-                    message: "OK",
-                    balance: {
-                        amounts: {
-                            ars: totalEnPesos,
-                            usd: totalEnPesos / dolar
-                        }
+            //hace el balance general
+            transactions[i].currency === "ARS" ?
+                (totalEnPesos += parseFloat(transactions[i].amount)) :
+                (totalEnPesos += parseFloat(transactions[i].amount) * dolar);
+        }
+    }
+
+    //Si el status es 500
+    if (statusCode === 500) {
+        response = codeResponse(500, "Internal Server Error");
+    } else {
+        //si la operacion fue exitosa
+        response = {
+            body: JSON.stringify({
+                status: 200,
+                message: "OK",
+                balance: {
+                    amounts: {
+                        ars: totalEnPesos,
+                        usd: totalEnPesos / dolar
                     }
-                })
-            }
+                }
+            })
         }
     }
 }
+
+module.exports = { handler };
